d50edd9440b1c6814adc9c48d9f7d626f4a5c474
uryonet <uryonet@gmail.com>
2018-10-17 22:20:12 +0900

    アダプターファイルを追加

--- app/src/main/java/com/uryonet/movify/presenter/view/activity/MainActivity.java
+++ app/src/main/java/com/uryonet/movify/presenter/view/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.uryonet.movify;
+package com.uryonet.movify.presenter.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.uryonet.movify.R;
 
 public class MainActivity extends AppCompatActivity {
 
